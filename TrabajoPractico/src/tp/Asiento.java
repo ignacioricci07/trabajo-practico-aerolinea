58d93e586ebe90ba8fb2b41efb3eb93789424e13
Soy Nacho <nachoricci07@gmail.com>
2024-11-04 17:39:56 -0300

    agregando clases

--- TrabajoPractico/src/tp/Asiento.java
+++ TrabajoPractico/src/tp/Asiento.java
@@ -1,5 +1,21 @@
 package tp;
 
 public class Asiento {
+	
+	int numero;
+	Pasajero pasajero;
+	boolean disponible;
+	int seccion;
+	
+	public Asiento(int numero, int seccion) {
+		this.numero = numero;
+		this.seccion = seccion;
+		this.disponible = true;
+		this.pasajero = null;
+	}
+	
+	public boolean estaDisponible() {
+		return disponible;
+	}
 
 }
